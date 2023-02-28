856610aa51f8e587c213d13714369a4642ebec00
mursalhabib <mursal.habib27@gmail.com>
2023-02-28 20:35:25 +0700

    Designing UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,110 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter, Rubik } from "@next/font/google";
+import styles from "./page.module.css";
+
+const rubik = Rubik({ subsets: ["latin"], variable: "--font-rubik" });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-full m-8">
+      <div className="flex justify-between sm:px-2 flex-col md:flex-row gap-4">
+        <div className="flex-1 ">
+          <div className="h-52 bg-gradient-to-r from-cyan-500 to-blue-700 p-8 rounded-3xl transition ease-in-out hover:-translate-y-1 hover:scale-105">
+            <h1
+              className={`${rubik.variable} font-sans text-lg font-light text-white`}
+            >
+              Uang Sekarang
+            </h1>
+            <h1
+              className={`${rubik.variable} font-sans text-xl font-bold text-white`}
+            >
+              Rp. 10.000.000
+            </h1>
+          </div>
+          <div className="flex justify-between my-6 gap-4">
+            <button
+              className={`${rubik.variable} font-sans flex-1 border border-blue-700 border-2 hover:bg-blue-700 hover:text-white bg-white rounded-full border-blue-700 py-3 text-blue-700`}
+            >
+              Uang Masuk
+            </button>
+            <button
+              className={`${rubik.variable} font-sans flex-1 border border-gray-700 border-2 hover:bg-gray-700 hover:text-white bg-white rounded-full border-gray-700 py-3 text-gray-700`}
+            >
+              Uang Keluar
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="flex-1">
+          <div className="flex justify-start">
+            <h1
+              className={`${rubik.variable} font-sans text-xl font-bold mb-4 flex-1`}
+            >
+              Histori
+            </h1>
+          </div>
+          <div>
+            <div className="bg-gray-100 p-2 rounded-xl mb-2">
+              <div className="flex justify-between">
+                <div>
+                  <p
+                    className={`${rubik.variable} font-sans text-sm font-semibold`}
+                  >
+                    Nama pemasukan
+                  </p>
+                  <p className={`${rubik.variable} font-sans text-sm`}>
+                    Tanggal
+                  </p>
+                </div>
+                <div
+                  className={`${rubik.variable} font-sans text-md font-black text-green-700`}
+                >
+                  + 5.000.000
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-100 p-2 rounded-xl mb-2">
+              <div className="flex justify-between">
+                <div>
+                  <p
+                    className={`${rubik.variable} font-sans text-sm font-semibold`}
+                  >
+                    Nama pemasukan
+                  </p>
+                  <p className={`${rubik.variable} font-sans text-sm`}>
+                    Tanggal
+                  </p>
+                </div>
+                <div
+                  className={`${rubik.variable} font-sans text-md font-black text-green-700`}
+                >
+                  10.000.000
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-100 p-2 rounded-xl mb-2">
+              <div className="flex justify-between">
+                <div>
+                  <p
+                    className={`${rubik.variable} font-sans text-sm font-semibold`}
+                  >
+                    Nama pengeluaran
+                  </p>
+                  <p className={`${rubik.variable} font-sans text-sm`}>
+                    Tanggal
+                  </p>
+                </div>
+                <div
+                  className={`${rubik.variable} font-sans text-md font-black text-red-700`}
+                >
+                  - 1.000.000
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
